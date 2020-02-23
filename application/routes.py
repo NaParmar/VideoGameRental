a46e507ed4b25b7dd5f5bde0bda2888d4d6996a0
NaParmar <namneet.parmar@qa.com>
2020-02-23 17:03:43 +0000

    Added routes for the register and login pages and updated the layout template with hyperlinks

--- application/routes.py
+++ application/routes.py
@@ -9,3 +9,11 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@app.route('/register')
+def register():
+    return render_template('register.html', title='Register')
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Login')
